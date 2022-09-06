619f045a414fdbb2249733a330cef531e8c42839
Alfredo Blanco <alfre.blanco@icloud.com>
2022-09-06 19:02:07 -0300

    Starting

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,9 +3,10 @@ import styles from '../styles/Home.module.css';
 
 export default function NavBar(){
     return (
-        <ul className={styles.nav}>
-            <Link href='#Presentation' passHref>
-                <li>Presentation</li>
+        <ul className='fixed top-0 left-0 flex flex-col items-center justify-around
+            h-screen w-max bg-sky-200 ' >
+            <Link href='#About' passHref>
+                <li>About</li>
             </Link>
             <Link href='#Tech' passHref>
                 <li>Technologies</li>
--- Components/Present.tsx
+++ Components/Present.tsx
@@ -1,17 +1,46 @@
-import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 
 export default function Present(){
+    const [english, setEnglish] = useState(false);
+
     return (
-        <div className={`${styles.main} ${styles.present}`} id='Presentation'>
-            <h1>Hi, my name is Alfredo Bco.</h1>
+        <div className='flex flex-col items-center justify-center h-screen w-full ' id='About'>
+            <button className='fixed top-10 right-5 bg-black w-max h-max p-4 text-white'
+            onClick={() => setEnglish(!english)} >X</button>
             
-            <p>
-                {"I'm"} a passionate Argentinian developer who wants to keep learning new things every day.<br />
-                This IT word is huge and I want to know most of it.<br/>
-                Thats why {"I'm"} always looking for challenges and dare myself.
-            </p>
-            <span>{"Let's"} know more about me...</span>
+            <h1 className='text-center font-bold text-2xl p-4 border rounded-full' >
+                {
+                    english
+                    ? 'Hi, my name is '
+                    : 'Hola, mi nombre es '
+                } Alfredo Blanco.
+            </h1>
+            {
+                english
+                ? (
+                    <p>
+                        {"I'm"} a passionate Argentinian developer who wants to keep learning new things every day.<br />
+                        This IT word is huge and I want to know most of it.<br/>
+                        Thats why {"I'm"} always looking for challenges and dare myself.
+                    </p>
+                )
+                : (
+                    <p>
+                        Soy un desarrollador Argentino, apasionado por aprender cosas nueva todos los días. <br />
+                        El mundo IT es enorme y me gustaría conocerlo todo. <br />
+                        Es por eso que siempre estoy desafiandome a mi mismo.
+                    </p>
+                )
+            }
+            <span>
+                {
+                    english
+                    ? "Let's know more about me..."
+                    : 'Veamos un poco más sobre mi...'
+                }
+            </span>
+            
         </div>
     )
 }
