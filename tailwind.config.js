e8380ab0c7b7d0065c5ca99df90accf2d45f843f
Alfredo Blanco <alfre.blanco@icloud.com>
2022-10-24 18:34:24 -0300

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      screens : {
-        'xs' : '300px',
-        'xsm' : '500px',
-        'xlg' : '900px'
-      },
-      keyframes: {
-        marquee : {
-          '0%' : { left: '0' },
-          '100%' : { left: '-100%' }
-        },
-        appear : {
-          '0%' : { opacity : '0' },
-          '100%' : { opacity : '50' }
-        }
-      },
-      animation: {
-        infScroll : 'marquee 20s linear infinite',
-        appear : 'appear 2s'
-      }
-    },
+    extend: {},
   },
   plugins: [
     require('tailwind-scrollbar-hide')
