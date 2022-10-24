582c06e75e126ab628320eaa3d22050ca5331706
Alfredo Blanco <alfre.blanco@icloud.com>
2022-10-24 20:29:24 -0300

    Fiished

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,30 +29,3 @@ const Home: NextPage = () => {
 }
 
 export default Home
-
-/* theme: {
-    extend: {
-      screens : {
-        'xs' : '300px',
-        'xsm' : '500px',
-        'xlg' : '900px'
-      },
-      keyframes: {
-        marquee : {
-          '0%' : { left: '0' },
-          '100%' : { left: '-100%' }
-        },
-        appear : {
-          '0%' : { opacity : '0' },
-          '100%' : { opacity : '50' }
-        }
-      },
-      animation: {
-        infScroll : 'marquee 20s linear infinite',
-        appear : 'appear 2s'
-      }
-    },
-  },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ], */
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,30 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens : {
+        'xs' : '300px',
+        'xsm' : '500px',
+        'xlg' : '900px'
+      },
+      keyframes: {
+        marquee : {
+          '0%' : { left: '0' },
+          '100%' : { left: '-100%' }
+        },
+        appear : {
+          '0%' : { opacity : '0' },
+          '100%' : { opacity : '50' }
+        }
+      },
+      animation: {
+        infScroll : 'marquee 20s linear infinite',
+        appear : 'appear 2s'
+      }
+    },
   },
   plugins: [
     require('tailwind-scrollbar-hide')
